07bd0a2b1a51926115f497b50bde7e5595f96cd4
jsuisgood2011 <jsuisgood@gmail.com>
2017-09-11 17:22:32 +0800

    add filter function

--- preProcess.py
+++ preProcess.py
@@ -18,7 +18,7 @@ def imGray(filePath):
       return im_gray
 def imBinary(im_gray):
     
-     retval, im_at_fixed = cv2.threshold(im_gray,199, 255, cv2.THRESH_BINARY) 
+     retval, im_at_fixed = cv2.threshold(im_gray,200, 255, cv2.THRESH_BINARY) 
          #将阈值设置为123，阈值类型为cv2.THRESH_BINARY，则灰度在大于123的像素其值将设置为255，其它像素设置为0
      #cv2.axis("off") 
      #cv2.title("Fixed Thresholding")
@@ -26,6 +26,12 @@ def imBinary(im_gray):
      cv2.waitKey(0) 
      cv2.destroyAllWindows()
      return im_at_fixed
+def filterAvg(im_gray):
+    blur = cv2.GaussianBlur(im_gray,(5,5),0) 
+    cv2.imshow('filter',blur)
+    cv2.waitKey(0) 
+    cv2.destroyAllWindows()
+    return blur
 def main():
     base_dir = "C:\Users\i073072\Downloads\OCR"
              #path_test_image = os.path.join(base_dir, "test_data.png")  
@@ -34,6 +40,8 @@ def main():
     im_gray = imGray(path_test_image)
     
     im_bin = imBinary(im_gray)
+    
+    im_blur = filterAvg(im_bin)
      
 if __name__ == '__main__':
     main()
